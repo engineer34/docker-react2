cebfe13381368e5b58658f9bb608ac2656e45316
DESKTOP-L7N8D4Q\Kafein <tugba.korkmaz@consultant.turkcell.com.tr>
2022-05-24 03:54:36 +0300

    tugba

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+class App extends Component{
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Tugba <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +22,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
