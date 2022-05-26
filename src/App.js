17a9bc12cc12ebcacfcd7b519f5f17c277515993
queENgineer <queENgineer>
2022-05-26 19:29:06 +0300

    change test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-    <h1> Learn React Tugba was changed </h1>
+    <h1> Learn React Tugba was changed on feature branch </h1>
       </header>
       <p className='App-intro'>HI there Tugba test!</p>
  
